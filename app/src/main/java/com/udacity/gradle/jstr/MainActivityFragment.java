7110ef795b88e615515531170c4a6b1e44cffbfa
Mateusz Widuch <matt.widuch@gmail.com>
2016-07-16 21:43:44 +0100

    docs: Add comment explaining empty constructor in Fragment

--- app/src/main/java/com/udacity/gradle/jstr/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/jstr/MainActivityFragment.java
@@ -1,7 +1,7 @@
 package com.udacity.gradle.jstr;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +16,8 @@ import com.google.android.gms.ads.AdView;
 public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
+        // All subclasses of Fragment must include a public empty constructor. For more details, visit
+        // https://developer.android.com/reference/android/app/Fragment.html
     }
 
     @Override
