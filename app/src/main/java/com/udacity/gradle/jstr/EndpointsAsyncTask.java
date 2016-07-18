4169a9a233b81dd600cba567a2ab08401ec43e68
Mateusz Widuch <matt.widuch@gmail.com>
2016-07-18 15:37:52 +0100

    feat: Modify EndpointAsyncTask to connect with deployed app engine

--- app/src/main/java/com/udacity/gradle/jstr/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/jstr/EndpointsAsyncTask.java
@@ -7,8 +7,6 @@ import android.support.v4.util.Pair;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
-import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 
 import java.io.IOException;
 
@@ -28,18 +26,8 @@ class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> 
     @Override
     protected String doInBackground(Pair<Context, String>... params) {
         if (JokesApiService == null) {  // Only do this once
-            JokesApi.Builder builder = new JokesApi.Builder(AndroidHttp.newCompatibleTransport(),
-                    new AndroidJsonFactory(), null)
-                    // options for running against local devappserver
-                    // - 10.0.2.2 is localhost's IP address in Android emulator
-                    // - turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                        @Override
-                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                            abstractGoogleClientRequest.setDisableGZipContent(true);
-                        }
-                    });
+            JokesApi.Builder builder = new JokesApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                    .setRootUrl("https://jstr-1376.appspot.com/_ah/api/");
             // end options for devappserver
 
             JokesApiService = builder.build();
