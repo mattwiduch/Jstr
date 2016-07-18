24f4220126665a91caf680bd8d05db7962c0c183
Mateusz Widuch <matt.widuch@gmail.com>
2016-07-18 20:45:05 +0100

    feat: Add Activity and Fragment to paid flavour

---
+++ app/src/paid/java/eu/redray/jstr/MainActivityFragment.java
@@ -0,0 +1,25 @@
+package com.udacity.gradle.jstr;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+        // All subclasses of Fragment must include a public empty constructor. For more details, visit
+        // https://developer.android.com/reference/android/app/Fragment.html
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
