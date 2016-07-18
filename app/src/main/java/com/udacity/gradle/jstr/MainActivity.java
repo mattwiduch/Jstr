4029616a919a4e547cea3fbf05a489694b300149
Mateusz Widuch <matt.widuch@gmail.com>
2016-07-18 14:38:40 +0100

    feat: Add EndpointAsyncTask template

--- app/src/main/java/com/udacity/gradle/jstr/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/jstr/MainActivity.java
@@ -1,7 +1,9 @@
 package com.udacity.gradle.jstr;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -49,10 +51,11 @@ public class MainActivity extends AppCompatActivity {
      * @param view that will display the toast
      * */
     public void tellJoke(View view) {
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
         Intent intent = new Intent(this, ShowJokeActivity.class);
         intent.putExtra(MainActivity.JOKE_KEY, (new JokeTeller()).tellJoke());
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
+        //startActivity(intent);
     }
 
 
