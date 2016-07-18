54242f2a8857e09bd3b5810205916cb4b0b3c0b0
Mateusz Widuch <matt.widuch@gmail.com>
2016-07-18 15:23:40 +0100

    feat: Modify GCE Endpoint to pull jokes from library

--- backend/src/main/java/eu/redray/jstr/backend/MyEndpoint.java
+++ backend/src/main/java/eu/redray/jstr/backend/MyEndpoint.java
@@ -10,13 +10,13 @@ import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 
-import javax.inject.Named;
+import eu.redray.JokeTeller;
 
 /**
  * An endpoint class we are exposing
  */
 @Api(
-        name = "myApi",
+        name = "jokesApi",
         version = "v1",
         namespace = @ApiNamespace(
                 ownerDomain = "backend.jstr.redray.eu",
@@ -27,13 +27,12 @@ import javax.inject.Named;
 public class MyEndpoint {
 
     /**
-     * A simple endpoint method that takes a name and says Hi back
+     * A simple endpoint method that sends a joke fetched from Java library
      */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
+    @ApiMethod(name = "getJoke")
+    public MyBean getJoke() {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
-
+        response.setData((new JokeTeller()).tellJoke());
         return response;
     }
 
